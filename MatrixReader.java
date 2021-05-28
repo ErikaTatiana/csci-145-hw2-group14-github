2614abc6a95ffe9f68aa94fe554f549db69fc25b
Erika Piltz <erikapiltzoliveiros@gmail.com>
2021-05-27 23:16:24 -0700

    actual work and not copying gitlab

--- MatrixReader.java
+++ MatrixReader.java
@@ -1,11 +1,46 @@
 package com.company;
 
 import java.util.Scanner;
-
+import java.io.File;
+import java.io.FileNotFoundException;
+// Erika, gitlab test
 public class MatrixReader {
-//testing gitLab
     public SparseMatrix read(String file){
+        try {
+            File matrixFile = new File(file);
+            Scanner readFile = new Scanner(matrixFile);
+
+            int numberRows = Integer.parseInt(readFile.next());
+            int numberColumns = Integer.parseInt(readFile.next());
+            SparseMatrix sparseMatrix = new SparseMatrix(numberRows,numberColumns);
+
+            int rowCounter = 1;
+
+            while(readFile.hasNextLine()){
+                String [] spaceArray = readFile.nextLine().split(" ");
+
+                for(int i = 0; i < spaceArray.length; i++){
+                    String [] commaArray = spaceArray[i].split(",");
+
+                    if(commaArray[0].equals("") || commaArray[commaArray.length-1].equals("")){
+                        break;
+                    }
+
+                    int column = Integer.parseInt(commaArray[0]);
+                    int value = Integer.parseInt(commaArray[commaArray.length-1]);
+
+                    sparseMatrix.insert(rowCounter, column, value);
+
+                    rowCounter++;
+                }
+            }
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File not found. ");
+        }
+
         return null;
     }
 }
+
 
